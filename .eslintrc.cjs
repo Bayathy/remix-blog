27932585cd4315ec68431764f1992a1d45064160
Bayathy <misosoup0321@icloud.com>
2023-12-13 17:00:11 +0900

    feat: 🚀 環境構築しなおし

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,11 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
+  extends: [
+    "@bayathy/eslint-config",
+    "@bayathy/eslint-config-react",
+    "@bayathy/eslint-config-tailwindcss",
+  ],
+  rules: {
+    "unicorn/filename-case": "off",
+  },
 };
