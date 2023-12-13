7d98a0c39a03f64285a91285e71a56f805f2e16e
Bayathy <misosoup0321@icloud.com>
2023-12-13 17:09:09 +0900

    wip

--- app/root.tsx
+++ app/root.tsx
@@ -1,33 +1,46 @@
-import type { LinksFunction } from "@remix-run/cloudflare";
+import { NextUIProvider } from "@nextui-org/react";
 import { cssBundleHref } from "@remix-run/css-bundle";
 import {
-	Links,
-	LiveReload,
-	Meta,
-	Outlet,
-	Scripts,
-	ScrollRestoration,
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useNavigate,
 } from "@remix-run/react";
 
+import stylesheet from "./globals.css";
+
+import type { LinksFunction } from "@remix-run/cloudflare";
+
 export const links: LinksFunction = () => [
-	...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  { rel: "stylesheet", href: stylesheet },
+  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
 export default function App() {
-	return (
-		<html lang="en">
-			<head>
-				<meta charSet="utf-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<Meta />
-				<Links />
-			</head>
-			<body>
-				<Outlet />
-				<ScrollRestoration />
-				<Scripts />
-				<LiveReload />
-			</body>
-		</html>
-	);
+  const navigate = useNavigate();
+  return (
+    <html lang="ja" suppressHydrationWarning>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <NextUIProvider navigate={navigate}>
+          <div className="grid min-h-screen w-screen grid-cols-[1fr] grid-rows-[auto_1fr_auto] overflow-hidden">
+            <Header />
+            <Outlet />
+            <Footer />
+          </div>
+          <ScrollRestoration />
+          <Scripts />
+          <LiveReload />
+        </NextUIProvider>
+      </body>
+    </html>
+  );
 }
